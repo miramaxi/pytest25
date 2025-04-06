fcffb35aa18a92942f0fd13cdb165ccae43efe48
miramaxi <151769568+miramaxi@users.noreply.github.com>
2025-04-06 19:32:23 +0300

    Delete statistics.mode.py

--- statistics.mode.py
+++
@@ -1,6 +0,0 @@
-import statistics
-print(statistics.mode([1, 3, 3, 3, 5, 7, 7, 9]))
-print(statistics.mode(['red', 'red', 'green', 'yellow', 'white'])) 
-print(statistics.mode([-1, -3, 3, 3, 5, 7, 7, 9]))
-      
-      
